10ce775d7fafe64a4357431e61751ba07766e771
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-18 10:07:02 +0000

    added more seed data and added week column

--- db/seed-data.js
+++ db/seed-data.js
@@ -1,14 +1,122 @@
 export const links = [
   {
-    name: "google",
-    link: "https://www.google.com/",
-  },
-  {
-    name: "mdn docs",
-    link: "https://developer.mozilla.org/en-US/",
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 1,
   },
   {
     name: "placeholder",
     link: "www.thisisaplaceholder.com",
+    week: 1,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 2,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 2,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 3,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 3,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 4,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 4,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 5,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 5,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 6,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 6,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 7,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 7,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 8,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 8,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 9,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 9,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 10,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 10,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 11,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 11,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 12,
+  },
+  {
+    name: "placeholder",
+    link: "www.thisisaplaceholder.com",
+    week: 12,
   },
 ];
