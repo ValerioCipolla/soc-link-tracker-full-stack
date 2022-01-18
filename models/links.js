e867ac5b43064d572de52dcdec5486eade464292
Macsen Sully-Morgan <macsullymorgan@gmail.com>
2022-01-18 10:53:52 +0000

    post request for api written

--- models/links.js
+++ models/links.js
@@ -9,3 +9,8 @@ export async function getLinksByWeek(week) {
   const data = await query(`SELECT * FROM links WHERE week = $1`, [week]);
   return data.rows;
 }
+
+export async function createLink(newLink) {
+  const data = await query(`INSERT INTO links (name, link, week) VALUES ($1, $2, $3) RETURNING *`, [newLink.name, newLink.link, newLink.week]);
+  return data.rows;
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { getAllLinks, getLinksByWeek } from "../models/links.js";
+import { getAllLinks, getLinksByWeek, createLink } from "../models/links.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import logger from "morgan";
+import { create } from "domain";
 
 const router = express.Router();
 
@@ -24,19 +25,23 @@ app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use("/api", router);
 
+//Default test greeting.
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from us!" });
 });
 
+//Default test greeting.
 app.get("/api/greeting", async (req, res) => {
   res.json({ message: "Hello to you" });
 });
 
+//Find all links in the table.
 app.get("/api/links", async (req, res) => {
   const result = await getAllLinks();
   res.json({ success: true, message: "links found :)", payload: result });
 });
 
+//Find links by week, with alternate no weeks found response.
 app.get("/api/links/week/:week", async (req, res) => {
   const week = req.params.week;
   const result = await getLinksByWeek(week);
@@ -54,10 +59,23 @@ app.get("/api/links/week/:week", async (req, res) => {
   });
 });
 
+//Post request to database for new links.
+app.post("/api/links", async (req, res) => {
+  const newLink = req.body;
+  const result = await createLink(newLink);
+  res.json({
+    success: true,
+    message: `link created in week ${newLink.week} with name ${newLink.name}`,
+    payload: result,
+  })
+})
+
+//Responds to a request with no response with the api's home page.
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
+//displays the port that the server is listening on.
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
