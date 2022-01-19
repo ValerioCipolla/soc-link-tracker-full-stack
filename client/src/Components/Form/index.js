b4bacc5f639c82d6f006e0fa7cdda790cbdec5b8
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-19 10:34:25 +0000

    clean up after post request implemented

--- client/src/Components/Form/index.js
+++ client/src/Components/Form/index.js
@@ -5,7 +5,7 @@ const Form = () => {
   const [name, setName] = useState("");
   const [link, setLink] = useState("");
   const [week, setWeek] = useState("1");
-  
+
   // async function postData(name, link, week) {
   //   const data = {name:name, link: link, week: week}
   //   const res = await fetch("/api/links", {method: "POST", body: JSON.stringify(data)});
@@ -16,9 +16,16 @@ const Form = () => {
   async function handleSubmit(e) {
     e.preventDefault();
     console.log("submit button log");
-    console.log(name, link, week)
+    console.log(name, link, week);
     // postData(name, link, week);
-    await fetch("/api/links", {method:"POST", headers: {"Content-Type":"application/json"}, body: JSON.stringify({name: name, link: link, week: week})});
+    await fetch("/api/links", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: name, link: link, week: week }),
+    });
+    setName("");
+    setLink("");
+    alert("Resource created successfully!");
   }
 
   function handleChange(e) {
@@ -32,32 +39,44 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
-      <input name="name" type="text" onChange={handleChange}></input>
-      <label htmlFor="link">URL:</label>
-      <input name="link" type="url" onChange={handleChange}></input>
-      <label htmlFor="week">Pick a week:</label>
-      <select name="week" onChange={handleChange}>
-        <option value="1">Week 1</option>
-        <option value="2">Week 2</option>
-        <option value="3">Week 3</option>
-        <option value="4">Week 4</option>
-        <option value="5">Week 5</option>
-        <option value="6">Week 6</option>
-        <option value="7">Week 7</option>
-        <option value="8">Week 8</option>
-        <option value="9">Week 9</option>
-        <option value="10">Week 10</option>
-        <option value="11">Week 11</option>
-        <option value="12">Week 12</option>
-        <option value="13">Week 13</option>
-        <option value="14">Week 14</option>
-        <option value="15">Week 15</option>
-        <option value="16">Week 16</option>
-      </select>
-      <button type="submit">SUBMIT</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          name="name"
+          type="text"
+          onChange={handleChange}
+          value={name}
+        ></input>
+        <label htmlFor="link">URL:</label>
+        <input
+          name="link"
+          type="url"
+          onChange={handleChange}
+          value={link}
+        ></input>
+        <label htmlFor="week">Pick a week:</label>
+        <select name="week" onChange={handleChange} value={week}>
+          <option value="1">Week 1</option>
+          <option value="2">Week 2</option>
+          <option value="3">Week 3</option>
+          <option value="4">Week 4</option>
+          <option value="5">Week 5</option>
+          <option value="6">Week 6</option>
+          <option value="7">Week 7</option>
+          <option value="8">Week 8</option>
+          <option value="9">Week 9</option>
+          <option value="10">Week 10</option>
+          <option value="11">Week 11</option>
+          <option value="12">Week 12</option>
+          <option value="13">Week 13</option>
+          <option value="14">Week 14</option>
+          <option value="15">Week 15</option>
+          <option value="16">Week 16</option>
+        </select>
+        <button type="submit">SUBMIT</button>
+      </form>
+    </>
   );
 };
 
