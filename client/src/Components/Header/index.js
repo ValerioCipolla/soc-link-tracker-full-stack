eb344b5859d57cec3aef1aea2e0aee1be5f612ad
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-19 12:15:09 +0000

    header repositioned

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import "./header.css"
+import "./header.css";
+import Button from "../Button";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div id="left">
-      <h1>School of Code <br/>Link Tracker</h1>
-      <h3>A website to display links posted on the course in an easy to read format</h3>
+    <div id="header">
+      <h2>School of Code Link Tracker</h2>
+      <Link to="Form">
+        <Button buttonText="Add New Resource" />
+      </Link>
     </div>
   );
 };
@@ -14,7 +19,6 @@ export default Header;
 // if (x.style.display === "none"){
 //   x.style.display = "none"
 
-  
 // }
 // else{
 //   x.style.display = "none"
