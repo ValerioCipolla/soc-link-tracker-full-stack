ecc90e4209ad5d5abbc56483dc53d02bde7d4234
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-19 11:53:19 +0000

    fixed week numbers

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,29 +1,29 @@
 import React from "react";
 import ListItem from "../ListItem";
-import "./navbar.css"
+import "./navbar.css";
 
 const Navbar = ({ dispatch, fetchdata }) => {
   return (
     <ul id="navBar">
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={1} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={2} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={3} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={4} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={5} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={6} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={6} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={1} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={2} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={3} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={4} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={5} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={6} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={6} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={1} />
-      <ListItem dispatch={dispatch} fetchdata = {fetchdata} weekNumber={2} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={1} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={2} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={3} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={4} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={5} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={6} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={6} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={7} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={8} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={9} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={10} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={11} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={12} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={13} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={14} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={15} />
+      <ListItem dispatch={dispatch} fetchdata={fetchdata} weekNumber={16} />
     </ul>
   );
 };
 
 export default Navbar;
-
